599c7b2f890f6321ab5099f1fe4982248cc42258
Amr Khamis <101368138+AmrKhamis1@users.noreply.github.com>
2025-05-19 08:16:22 +0300

    update

--- myApp.js
+++ myApp.js
@@ -1,39 +1,32 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+const bodyParser = require("body-parser");
 
+require("dotenv").config();
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/", (req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+// app.post("/", (req, res,next) => {
+//   res.sendFile(__dirname + "/views/index.html");
+//   console.log("Hello Express");
+// });
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.post("/name", (req, res, next) => {
+  var first = req.query.first;
+  var last = req.query.last;
+  res.json({ name: first + " " + last });
+});
+module.exports = app;
